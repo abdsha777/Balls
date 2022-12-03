d836277be240a2000e7fe4f6cb0b3d7650ef013a
abdsha777 <94749392+abdsha777@users.noreply.github.com>
2022-12-03 23:13:31 +0530

    Add files via upload

--- class.js
+++ class.js
@@ -1,3 +1,5 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 const gravity=0.4;
 class Circle{   
     constructor(x,y){
@@ -9,23 +11,23 @@ class Circle{
     }
     draw(){
         c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,360,false);
-        c.fillStyle="#420039";
+        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        c.fillStyle="rgb(32, 17, 133)";
         c.fill();
     }
     change(dir){
         switch(dir){
             case 'w':
-                this.dy=-15;
+                this.dy=-20;
                 break;
             case 's':
                 this.dy+=5;
                 break;
             case 'a':
-                this.dx=-10;
+                this.dx=-5;
                 break;
             case 'd':
-                this.dx=10;
+                this.dx=5;
                 break;
         }
     }
@@ -53,11 +55,46 @@ class Circle{
         else{
             this.dx=0;
         }
-//      side walls
+
         if(this.x+this.radius> canvas.width || this.x -this.radius < 0){
-            this.dx=-this.dx;
+            this.dx=-this.dx
         }
         
+
+        this.x+=this.dx;
+        this.y+=this.dy;
+        this.draw();
+    }
+}
+
+class Homelander extends Circle{
+    constructor(x,y){
+        super(x,y);
+        this.img= new Image;
+        this.img.src="./homeSprite.png";
+        this.width=100;
+        this.height=100;
+    }
+    draw(){
+        c.drawImage(this.img,this.x,this.y,this.width,this.height);
+    }
+    update(){
+        if(this.y +this.radius + this.dy + this.height/2< canvas.height){
+            this.dy+=gravity;
+        }
+        else{
+            this.dy=0;
+            // if(this.dy<=1){
+            //     this.dy=0;
+            // }
+            // else{
+            //     this.dy=-this.dy + gravity*3;
+            // }
+        }
+        if(this.x + this.width>= canvas.width || this.x <= 0){
+            this.dx=-this.dx;
+        }
+
         this.x+=this.dx;
         this.y+=this.dy;
         this.draw();
--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
 
 const btns= document.querySelectorAll('.btn');
 
 canvas.height=window.innerHeight
 canvas.width=window.innerWidth
 
-var circle1= new Circle(300,300);
+var home= new Homelander(200,200);
 
 var dirs=['w','a','s','d'];
 
 btns.forEach((btn,idx)=>{
     btn.addEventListener('click',()=>{
-        circle1.change(dirs[idx]);
+        home.change(dirs[idx]);
     })
 })
 
 window.addEventListener('keydown',(e)=>{
-    circle1.change(e.key);
+    home.change(e.key);
 })
 
 
@@ -25,7 +23,7 @@ window.addEventListener('keydown',(e)=>{
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height)
-    circle1.update();
+    home.update();
 }
 
 animate();
